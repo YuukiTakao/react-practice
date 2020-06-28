d3701c470ecba6461f22ce09140288d9ec2f3470
Yuuki Takao <takaaaao3@gmail.com>
2020-06-28 23:59:24 +0900

    create-react-app intro

---
+++ hello/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class App extends Component {
+  render() {
+    return <div className='App'><h2>塔をたてるときは、まず座って費用を計算しよう</h2></div>
+  }
+}
